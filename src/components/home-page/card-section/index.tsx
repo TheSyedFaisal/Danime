a86778e5692c483cd2d73bbeb8dea8bc64970b6a
TheSyedFaisal <fazalfaisal911@gmail.com>
2026-01-12 23:15:37 +0500

    responsive

--- src/components/home-page/card-section/index.tsx
+++ src/components/home-page/card-section/index.tsx
@@ -4,7 +4,7 @@ const CardSection = () => {
   return (
     <section className="px-4 md:px-6 container">
       <div
-        className=" mt-6 rounded-2xl relative overflow-hidden px-6 "
+        className=" mt-6 rounded lg:rounded-2xl relative overflow-hidden px-6 "
         style={{
           backgroundImage: "url('/Group-20.png')",
           backgroundSize: "cover",
@@ -22,15 +22,15 @@ const CardSection = () => {
         </div>
 
         {/* Heading */}
-        <h2 className="page-font text-center text-white text-4xl pt-12">
+        <h2 className="page-font text-center text-white text-xl md:text-4xl pt-12">
           NEW COLLECTION
         </h2>
 
         {/* MAIN GRID */}
-        <div className="grid grid-cols-1 lg:grid-cols-[400px_1fr] gap-8 my-14 items-stretch relative z-100">
+        <div className="grid grid-cols-1 lg:grid-cols-[400px_1fr] gap-8 my-10 items-stretch relative z-100">
           {/* LEFT BIG CARD */}
-          <div className="flex flex-col">
-            <div className="relative flex-1 rounded-2xl overflow-hidden bg-white ">
+          <div className="flex flex-col ">
+            <div className="flex flex-col min-h-[350px] lg:min-h-[636px] rounded-2xl overflow-hidden bg-white relative">
               <Image
                 src="/card-1-img-1.jpg"
                 alt=""
@@ -44,10 +44,10 @@ const CardSection = () => {
                 alt=""
                 width={240}
                 height={240}
-                className="absolute top-6 right-22 rotate-12 z-10"
+                className="absolute top-6 md:w-[300px] md:h-[300px] lg:w-[240px] lg:h-[240] right-0 lg:right-22 rotate-12 z-10"
               />
 
-              <div className="absolute top-7 -right-1 z-20 w-[500px] h-[600px]">
+              <div className="absolute top-7 -right-1 z-20 w-[500px] h-[600px] hidden lg:block">
                 <Image
                   src="/crop-image.png"
                   alt=""
@@ -56,27 +56,42 @@ const CardSection = () => {
                 />
               </div>
 
+              <div className="absolute z-20 w-[350px] h-[500px] h-[500px] md:w-[700px] md:h-[670px] -bottom-30 md:-bottom-50 -left-30 lg:hidden">
+                <Image
+                  src="/card-1-img-3.png"
+                  alt=""
+                  fill
+                  className="object-contain"
+                />
+              </div>
+
               <Image
                 src="/cardred.png"
                 alt=""
                 fill
-                className="object-cover z-30 pointer-events-none"
+                className="object-cover z-30 pointer-events-none hidden lg:block"
+              />  
+              <Image
+                src="/rectangel-2.png"
+                alt=""
+                fill
+                className=" z-30 pointer-events-none lg:hidden"
               />
             </div>
 
             {/* LEFT TEXT */}
-            <div className="mt-3">
-              <h4 className="page-font text-white text-lg tracking-wide">
+            <div className="mt-3 text-center lg:text-left">
+              <h4 className="page-font text-white text-sm md:text-lg tracking-wide">
                 CREATE CUSTOM
               </h4>
-              <button className="mt-2 page-font text-white text-xs border border-white px-4 py-2 rounded-lg">
+              <button className="mt-2 page-font text-white text-[8px] md:text-xs border border-white px-4 py-2 rounded-lg">
                 SHOP NOW
               </button>
             </div>
           </div>
 
           {/* RIGHT GRID */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 h-full">
+          <div className="grid grid-cols-2 gap-6 h-full">
             {[
               { title: "JACKETS", img: "/card-2.jpg" },
               { title: "TOTE BAGS", img: "/card-3.jpg" },
@@ -85,7 +100,7 @@ const CardSection = () => {
             ].map((item) => (
               <div key={item.title} className="flex flex-col">
                 <div
-                  className="bg-white rounded-2xl overflow-hidden flex-1
+                  className="bg-white rounded-2xl overflow-hidden
                "
                 >
                   <Image
@@ -93,17 +108,17 @@ const CardSection = () => {
                     alt={item.title}
                     width={200}
                     height={150}
-                    className="w-full h-[270px] object-cover transition-transform duration-500 ease-out
+                    className="w-full h-[210px] md:h-[270px] object-cover transition-transform duration-500 ease-out
                hover:scale-105 hover:shadow-xl"
                   />
                 </div>
 
                 {/* RIGHT TEXT */}
-                <div className="mt-3">
-                  <h4 className="page-font text-white text-lg tracking-wide">
+                <div className="mt-3 text-center lg:text-left">
+                  <h4 className="page-font text-white text-sm md:text-lg tracking-wide">
                     {item.title}
                   </h4>
-                  <button className="mt-2 page-font text-white text-xs border border-white px-3 py-1.5 rounded-md">
+                  <button className="mt-2 page-font text-white text-[8px] md:text-xs border border-white px-3 py-1.5 rounded-md">
                     SHOP NOW
                   </button>
                 </div>
--- src/components/home-page/hero-section/index.tsx
+++ src/components/home-page/hero-section/index.tsx
@@ -4,29 +4,29 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className="px-4 md:px-8 pt-10 md:pt-20 container  ">
-      <div className="flex items-center justify-between md:px-6 lg:px-25 ">
+    <div className="px-4 md:px-6 pt-8 md:pt-20 container  ">
+      <div className="flex items-center justify-between px-3 sm:px-22 lg:px-25 ">
         <div>
-          <h2 className="text-black text-lg md:text-2xl lg:text-[35px] leading-[100%] page-font ml-1 lg:ml-2">
+          <h2 className="text-black text-sm md:text-2xl lg:text-[35px] leading-[100%] page-font ml-1 lg:ml-2">
             Be the
           </h2>
-          <h1 className="secondary text-4xl md:text-6xl lg:text-[70px] leading-[70%] page-font">
+          <h1 className="secondary text-2xl md:text-6xl lg:text-[70px] leading-[70%] page-font">
             only
           </h1>
         </div>
         <div>
-          <h2 className="text-black text-lg md:text-2xl lg:text-[35px] leading-[100%] page-font">
+          <h2 className="text-black text-sm md:text-2xl lg:text-[35px] leading-[100%] page-font">
             one in the
           </h2>
-          <h1 className="secondary text-4xl md:text-6xl lg:text-[70px] leading-[70%] page-font">
+          <h1 className="secondary text-2xl md:text-6xl lg:text-[70px] leading-[70%] page-font">
             ROOM
           </h1>
         </div>
       </div>
 
-      <div className=" bg-[#D43642] w-full max-w-[1801px] h-[450px] md:h-[460px] mx-auto mt-10 mb-2 rounded-2xl relative px-6 flex flex-col md:flex-row justify-between items-center">
+      <div className=" bg-[#D43642] w-full max-w-[1801px] h-[178px] sm:h-[260] md:h-[460px] mx-auto mt-5 md:mt-10 mb-2 rounded lg:rounded-2xl relative px-4 md:px-6 flex justify-between items-center">
         {/* Left Background Image */}
-        <div className="absolute left-0 top-5 pointer-events-none rounded-l-2xl h-full">
+        <div className="absolute left-0 top-5 pointer-events-none rounded-l-2xl h-full hidden md:block">
           <Image
             src="/vector-1.svg"
             alt="Left Background"
@@ -37,7 +37,7 @@ const HeroSection = () => {
         </div>
 
         {/* Right Background Image */}
-        <div className="absolute right-0 top-5 pointer-events-none h-[120%]">
+        <div className="absolute right-0 top-5 pointer-events-none h-[120%] hidden md:block">
           <Image
             src="/vector-2.svg"
             alt="Right Background"
@@ -49,12 +49,12 @@ const HeroSection = () => {
 
         {/* TEXT (LEFT SIDE) */}
         <div className="flex items-end justify-between w-full mt-3 md:mt-0">
-          <div className="relative z-10  md:max-w-[350px] text-white space-y-3">
-            <div className=" md:mb-28">
-              <h2 className="text-xl md:text-3xl text-center md:text-left font-bold page-font mb-1.5 md:mb-3">
+          <div className="relative z-10  text-white space-y-3 ">
+            <div className="mb-10 md:mb-40 lg:mb-26">
+              <h2 className="text-[8px] sm:text-base md:text-2xl lg:text-3xl max-w-[90px] sm:max-w-[180px] md:max-w-[260px] lg:max-w-[350px] text-left font-bold page-font mb-1.5 md:mb-3">
                 Lorem ipsum dolor sit amet
               </h2>
-              <p className="text-xs md:text-lg text-center md:text-left md:max-w-[285px]">
+              <p className="text-[4px] sm:text-xs lg:text-base text-left max-w-[70px] sm:max-w-[200px] md:max-w-[200px] lg:max-w-[285px]">
                 Ditch the mass-produced merch. We create 1-of-1, hand-painted
                 denim masterpieces that ensure your style is as unique as your
                 fandom.
@@ -62,20 +62,20 @@ const HeroSection = () => {
             </div>
 
             <div>
-              <button className="border px-4 py-4 md:py-2 page-font text-xs w-full md:w-auto">
+              <button className="border px-2 md:px-4 py-1 md:py-2 lg:py-4 md:py-2 page-font text-[8px] md:text-xs w-auto">
                 shop now{" "}
               </button>
             </div>
           </div>
 
           <div>
-            <div className="relative z-100 bg-[#F54F5F] rounded-xl p-3 hidden md:block">
+            <div className="relative z-100 md:bg-[#F54F5F] rounded md:rounded-xl p-1 md:p-3 ">
             <div className="flex">
-              <Image src="/user.png" alt="" height={30} width={30} />
-              <Image src="/user.png" alt="" height={30} width={30} />
-              <Image src="/user.png" alt="" height={30} width={30} />
+              <Image src="/user.png" alt="" height={30} width={30} className="w-[10px] h-[10px] sm:w-[30px] sm:h-[30px]" />
+              <Image src="/user.png" alt="" height={30} width={30} className="w-[10px] h-[10px] sm:w-[30px] sm:h-[30px]" />
+              <Image src="/user.png" alt="" height={30} width={30} className="w-[10px] h-[10px] sm:w-[30px] sm:h-[30px]" />
             </div>
-            <p className="max-w-[150px] text-white mt-1 text-xs">
+            <p className="max-w-[50px] sm:max-w-[150px] text-white mt-1 text-[4px] sm:text-xs">
               Lorem ipsum dolor sit amet consectetur. Tempor ut quam arcu
               blandit laoreet.{" "}
             </p>
@@ -83,13 +83,13 @@ const HeroSection = () => {
           </div>
         </div>
 
-        <div className="md:absolute left-1/2 top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 sm:-translate-y-1/2 z-20">
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -translate-y-1/2 z-20 w-[155px] sm:w-auto">
           <Image
             src="/hero-imagee.png"
-            width={400}
-            height={400}
+            width={600}
+            height={600}
             alt="Hero Image"
-            className="w-[160px] sm:w-[190px] md:w-[400px] drop-shadow-2xl relative sm:top-[145.8px] md:-top-19"
+            className="w-[100%] sm:w-[190px] md:w-[300px] lg:w-[400px] drop-shadow-2xl relative -top-[30px] sm:-top-4 md:top-0 lg:-top-19 image"
             priority
           />
         </div>
