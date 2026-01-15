6e24abf478caaf90cd87008a9d275284f440d0d1
TheSyedFaisal <fazalfaisal911@gmail.com>
2026-01-16 00:38:09 +0500

    type safety error fixed

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,5 +1,12 @@
-// Button.jsx
-const ShopNowButton = ({ children, className = "", ...props }) => {
+// ShopNowButton.tsx
+import React, { ButtonHTMLAttributes, FC, ReactNode } from "react";
+
+interface ShopNowButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: string;
+  className?: string;
+}
+
+const ShopNowButton: FC<ShopNowButtonProps> = ({ children, className = "", ...props }) => {
   const baseStyle =
     "mt-2 page-font cursor-pointer text-white text-[8px] md:text-xs border border-white px-3 py-2 rounded-md hover:bg-foreground transition-all duration-300";
 
