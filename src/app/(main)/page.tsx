c315713ebb15948ac6e5328c7749a3fc0504c450
TheSyedFaisal <fazalfaisal911@gmail.com>
2026-06-21 22:29:31 +0500

    feat: enhance home page layout by integrating CollectionsSection and updating CardSection component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,21 +1,19 @@
-import Brand from '@/components/home-page/brand-story'
-import Features from '@/components/home-page/features-section'
-import HeroSection from '@/components/home-page/hero-section'
-import HotSellers from '@/components/home-page/hot-sellers'
-import CardSection from '../collections/page'
-
-
+import Brand from "@/components/home-page/brand-story";
+import Features from "@/components/home-page/features-section";
+import HeroSection from "@/components/home-page/hero-section";
+import HotSellers from "@/components/home-page/hot-sellers";
+import CollectionsSection from "@/components/collections/CollectionsSection";
 
 const Home = () => {
   return (
-    <div className='relative z-0 '>
+    <div className="relative z-0 ">
       <HeroSection />
-      <CardSection />
+      <CollectionsSection />
       <Features />
       <HotSellers />
       <Brand />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
